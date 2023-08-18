a196f499444e6eb98bb0f7fc279d6497c673b815
Subham Shrestha <98730562+shresthasubham@users.noreply.github.com>
2023-08-18 18:06:28 +0545

    Add files via upload

---
+++ game.py
@@ -0,0 +1,263 @@
+import pygame as pg
+from OpenGL.GL import *
+import numpy as np
+from OpenGL.GL.shaders import compileProgram, compileShader
+import pyrr
+import os
+
+from button import Button
+from pygame.locals import QUIT, KEYDOWN, K_q
+
+screenWidth = 1280
+screenHeight = 720
+aspectRatio = screenWidth/screenHeight
+
+
+def get_font(size):
+    return pg.font.Font("assets/font.ttf", size)
+
+
+class App:
+
+    def __init__(self):
+
+        pg.init()
+        pg.display.set_mode((screenWidth, screenHeight),
+                            pg.OPENGL | pg.DOUBLEBUF)
+        self.clock = pg.time.Clock()
+        self.quit_button = Button(image=pg.image.load("assets/Play Rect.png"), pos=(640, 650),
+                                  text_input="BACK", font=get_font(50), base_color="White", hovering_color="Green")
+
+        # initialize screen
+
+        glClearColor(0.0, 0.0, 0.0, 0)
+        glEnable(GL_BLEND)
+        glEnable(GL_DEPTH_TEST)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+        self.shader = self.createShader(os.path.join(os.path.dirname(
+            __file__), "shaders/vertex.txt"), os.path.join(os.path.dirname(__file__), "shaders/fragment.txt"))
+        glUseProgram(self.shader)
+        glUniform1i(glGetUniformLocation(self.shader, "imageTexture"), 0)
+
+        # cube object for position and rotation
+        self.cube = Cube(
+            position=[0, 0, -3],
+            eulers=[0, 0, 0]
+        )
+
+        self.cube_mesh = CubeMesh()
+
+        # matrix for projected view
+        projection_transform = pyrr.matrix44.create_perspective_projection(
+            fovy=45, aspect=screenWidth/screenHeight,
+            near=0.1, far=10, dtype=np.float32
+        )
+
+        glUniformMatrix4fv(
+            glGetUniformLocation(self.shader, "projection"),
+            1, GL_FALSE, projection_transform
+        )
+        self.modelMatrixLocation = glGetUniformLocation(self.shader, "model")
+
+        self.mainLoop()
+
+    def mainLoop(self):
+
+        running = True
+
+        while running:
+            for event in pg.event.get():
+                if event.type == QUIT:
+                    running = False
+                elif event.type == KEYDOWN:
+                    if event.key == K_q:  # Check for "Q" key press
+                        running = False
+
+                # Handle button events
+                self.quit_button.handle_event(event)
+            # clear screen
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+            glUseProgram(self.shader)
+
+            # updating rotation
+
+            # about X-axis
+            self.cube.eulers[0] += 0.25
+            if self.cube.eulers[0] > 360:
+                self.cube.eulers[0] -= 360
+
+            # about Y-axis
+            self.cube.eulers[2] += 0.25
+            if self.cube.eulers[2] > 360:
+                self.cube.eulers[2] -= 360
+            # about Z-axis
+            self.cube.eulers[1] += 0.25
+            if self.cube.eulers[1] > 360:
+                self.cube.eulers[1] -= 360
+
+            # rotation
+            model_transform = pyrr.matrix44.create_identity(dtype=np.float32)
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform,
+                m2=pyrr.matrix44.create_from_eulers(
+                    eulers=np.radians(self.cube.eulers),
+                    dtype=np.float32
+                )
+            )
+
+            # updating translation
+
+            # along X-axis
+            self.cube.position[0] += self.cube.xDirection*0.005
+            if self.cube.position[0] > 0.95*aspectRatio:
+                self.cube.xDirection = -1
+            if self.cube.position[0] < -0.95*aspectRatio:
+                self.cube.xDirection = 1
+            # along Y-axis
+            self.cube.position[1] += self.cube.yDirection*0.005
+            if self.cube.position[1] > 0.85:
+                self.cube.yDirection = -1
+            if self.cube.position[1] < -0.85:
+                self.cube.yDirection = 1
+
+            # sending it to its position
+            model_transform = pyrr.matrix44.multiply(
+                m1=model_transform,
+                m2=pyrr.matrix44.create_from_translation(
+                    vec=self.cube.position,
+                    dtype=np.float32
+                )
+            )
+
+            glUniformMatrix4fv(self.modelMatrixLocation, 1,
+                               GL_FALSE, model_transform)
+
+            # actually drawing the cube
+            glBindVertexArray(self.cube_mesh.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.cube_mesh.vertex_count)
+
+            pg.display.flip()
+
+            self.clock.tick(60)
+
+        self.quit()
+
+    def createShader(self, vertexFilepath, fragmentFilepath):
+        with open(vertexFilepath, 'r') as f:
+            vertex_src = f.readlines()
+
+        with open(fragmentFilepath, 'r') as f:
+            fragment_src = f.readlines()
+
+        shader = compileProgram(compileShader(vertex_src, GL_VERTEX_SHADER),
+                                compileShader(fragment_src, GL_FRAGMENT_SHADER))
+
+        return shader
+
+    def quit(self):
+
+        self.cube_mesh.destroy()
+        glDeleteProgram(self.shader)
+        pg.quit()
+
+
+class Cube:
+
+    def __init__(self, position, eulers) -> None:
+        self.position = np.array(position, dtype=np.float32)
+        self.eulers = np.array(eulers, dtype=np.float32)
+        self.xDirection = 1
+        self.yDirection = 1
+        self.zDirection = 1
+
+
+class CubeMesh:
+
+    def __init__(self):
+
+        # x,y,z,R,G,B
+        self.vertices = (
+            # front face
+            -0.3, -0.3, -0.3, 1.0, 1.0, 0.0,  # BottomLeftFront yellow
+            0.3, -0.3, -0.3, 0.0, 1.0, 1.0,  # BottomRightFront cyan
+            0.3,  0.3, -0.3, 0.0, 0.0, 1.0,  # TopRightFront blue
+
+            0.3,  0.3, -0.3, 0.0, 0.0, 1.0,  # TopRightFront blue
+            -0.3,  0.3, -0.3, 0.0, 1.0, 0.0,  # TopLeftFront green
+            -0.3, -0.3, -0.3, 1.0, 1.0, 0.0,  # BottomLeftFront yellow
+
+            # left face
+            -0.3, -0.3, -0.3, 1.0, 1.0, 0.0,  # BottomLeftFront yellow
+            -0.3, -0.3, 0.3, 0.0, 1.0, 1.0,  # BottomLeftBack cyan
+            -0.3,  0.3, 0.3, 0.0, 0.0, 1.0,  # TopLeftBack blue
+
+            -0.3,  0.3, 0.3, 0.0, 0.0, 1.0,  # TopLeftBack blue
+            -0.3,  0.3, -0.3, 0.0, 1.0, 0.0,  # TopLeftFront green
+            -0.3, -0.3, -0.3, 1.0, 1.0, 0.0,  # BottomLeftFront yellow
+
+            # back face
+            -0.3, -0.3, 0.3, 0.0, 1.0, 1.0,  # BottomLeftBack cyan
+            0.3, -0.3, 0.3, 1.0, 1.0, 0.0,  # BottomRightBack yellow
+            0.3,  0.3, 0.3, 0.0, 1.0, 0.0,  # TopRightBack green
+
+            0.3,  0.3, 0.3, 0.0, 1.0, 0.0,  # TopRightBack green
+            -0.3,  0.3, 0.3, 0.0, 0.0, 1.0,  # TopLeftBack blue
+            -0.3, -0.3, 0.3, 0.0, 1.0, 1.0,  # BottomLeftBack cyan
+
+            # right face
+
+            0.3, -0.3, -0.3, 0.0, 1.0, 1.0,  # BottomRightFront cyan
+            0.3, -0.3, 0.3, 1.0, 1.0, 0.0,  # BottomRightBack yellow
+            0.3,  0.3, 0.3, 0.0, 1.0, 0.0,  # TopRightBack green
+
+            0.3,  0.3, 0.3, 0.0, 1.0, 0.0,  # TopRightBack green
+            0.3,  0.3, -0.3, 0.0, 0.0, 1.0,  # TopRightFront blue
+            0.3, -0.3, -0.3, 0.0, 1.0, 1.0,  # BottomRightFront cyan
+
+            # top face
+            0.3,  0.3, -0.3, 0.0, 1.0, 1.0,  # TopRightFront cyan
+            0.3,  0.3, 0.3, 0.0, 1.0, 1.0,  # TopRightBack cyan
+            -0.3,  0.3, -0.3, 0.0, 1.0, 1.0,  # TopLeftFront cyan
+
+            -0.3,  0.3, -0.3, 0.0, 1.0, 1.0,  # TopLeftFront cyan
+            -0.3,  0.3, 0.3, 0.0, 1.0, 1.0,  # TopLeftBack cyan
+            0.3,  0.3, 0.3, 0.0, 1.0, 1.0,  # TopRightBack cyan
+
+            # bottom face
+            0.3, -0.3, -0.3, 0.0, 1.0, 1.0,  # BottomRightFront cyan
+            0.3, -0.3, 0.3, 0.0, 1.0, 1.0,  # BottomRightBack cyan
+            -0.3, -0.3, -0.3, 0.0, 1.0, 1.0,  # BottomLeftFront cyan
+
+            -0.3, -0.3, -0.3, 0.0, 1.0, 1.0,  # BottomLeftFront cyan
+            -0.3, -0.3, 0.3, 0.0, 1.0, 1.0,  # BottomLeftBack cyan
+            0.3, -0.3, 0.3, 0.0, 1.0, 1.0,  # BottomRightBack cyan
+
+
+        )
+
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+
+        self.vertex_count = len(self.vertices)//6
+        self.vao = glGenVertexArrays(1)  # vertex array object
+        glBindVertexArray(self.vao)
+
+        self.vbo = glGenBuffers(1)  # vertex buffer object
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes,
+                     self.vertices, GL_STATIC_DRAW)
+
+        glEnableVertexAttribArray(0)  # position
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)  # color
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+                              24, ctypes.c_void_p(12))
+
+    def destroy(self):
+        glDeleteVertexArrays(1, (self.vao,))
+        glDeleteBuffers(1, (self.vbo,))
+
+
+# if __name__ == "__main__":
+    # myApp = App()
---
+++ main.py
@@ -0,0 +1,246 @@
+
+import pygame
+import sys
+import os
+from button import Button  # Import  Button class
+from math import sin, cos, radians
+from random import choice, randrange, uniform
+from game import *
+
+
+# Initialize  pygame engine
+pygame.init()
+
+# Initialize  window
+SCREEN = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("Menu")
+
+# Load  background image
+BG = pygame.image.load("assets/Background.png")
+
+# Function to get font and size of button/menu fonts
+
+
+def get_font(size):
+    return pygame.font.Font("assets/font.ttf", size)
+
+######################################################################################
+# Function to handle PLAY1 screen/2D rendring
+
+
+def PLAY1():
+
+    # Define a class for animated symbols
+    class Symbol:
+        def __init__(self, angle, radius):
+            self.angle = angle
+            self.radius = radius
+            self.speed = 0.5  # Speed of rotation
+            self.value = choice(green_katakana)  # Random initial symbol
+            self.interval = 10  # Interval to change symbol
+
+        def draw(self, color):
+            frames = pygame.time.get_ticks()    # Get current time
+        # Change symbol if interval is reached
+            if not frames % self.interval:
+                self.value = choice(green_katakana if color ==
+                                    'green' else lightgreen_katakana)
+            self.angle += self.speed  # Update rotation angle
+            self.x = WIDTH // 2 + int(self.radius * cos(radians(self.angle)))
+            self.y = HEIGHT // 2 + int(self.radius * sin(radians(self.angle)))
+            surface.blit(self.value, (self.x, self.y))
+    #############################################################################
+    # Initialize screen and surface
+    RES = WIDTH, HEIGHT = 1280, 720
+    FONT_SIZE = 40
+    alpha_value = 0
+    screen = pygame.display.set_mode(RES)
+    surface = pygame.Surface(RES)
+    surface.set_alpha(alpha_value)
+    clock = pygame.time.Clock()
+
+    # Create lists of characters and their corresponding rendered fonts
+    katakana = [chr(int('0x30A0', 16) + i) for i in range(96)]
+    font = pygame.font.Font('assets/MSMINCHO.ttf', FONT_SIZE)
+    green_katakana = [font.render(
+        char, True, (40, randrange(160, 256), 40)) for char in katakana]
+    lightgreen_katakana = [font.render(
+        char, True, pygame.Color('lightgreen')) for char in katakana]
+
+    chain_count = 8
+    symbol_count = 25
+
+    symbol_chains = []
+
+    # Create multiple symbol chains
+    for chain_index in range(chain_count):
+        radius = uniform(50, min(WIDTH, HEIGHT) // 2)
+        symbols = [Symbol(i * (360 / symbol_count), radius)
+                   for i in range(symbol_count)]
+        symbol_chains.append(symbols)
+    #############################################################################
+
+    class RotatingColorChangingCircle:
+        def __init__(self):
+            self.angle = 0
+            self.speed = 1  # Adjust the rotation speed as needed
+            self.radius = 50
+            self.color = pygame.Color(255, 0, 0)  # Initial color
+
+        def update(self):
+            self.angle += self.speed
+            normalized_sine = (sin(radians(self.angle)) + 1) / \
+                2  # Normalize sine to [0, 1]
+            # Normalize cosine to [0, 1]
+            normalized_cosine = (cos(radians(self.angle)) + 1) / 2
+            self.color.r = int(255 * normalized_sine)  # Scale to [0, 255]
+            self.color.g = int(255 * normalized_cosine)  # Scale to [0, 255]
+
+        def draw(self):
+            center = ((WIDTH+40) // 2, (HEIGHT+30) // 2)
+            pygame.draw.circle(surface, self.color, center, self.radius)
+
+    rotating_circle = RotatingColorChangingCircle()
+    #############################################################################
+
+    class RotatingText:
+        def __init__(self, text, font_size):
+            self.angle = 0
+            self.speed = 1  # Adjust the rotation speed as needed
+            self.font = get_font(font_size)
+            self.text_surface = self.font.render(
+                text, True, pygame.Color("white"))
+
+        def update(self):
+            self.angle += self.speed
+
+        def draw(self):
+            rotated_text = pygame.transform.rotate(
+                self.text_surface, self.angle)
+            text_rect = rotated_text.get_rect(
+                center=((WIDTH+30) // 2, (HEIGHT+30) // 2))
+            surface.blit(rotated_text, text_rect)
+
+    rotating_circle = RotatingColorChangingCircle()
+
+    # Initialize rotating "BCT" text
+    rotating_text_BCT = RotatingText("BCT", 60)
+    #############################################################################
+    # Game loop
+    while True:
+        # Get mouse position
+        PLAY_MOUSE_POS = pygame.mouse.get_pos()
+
+        # Fill the screen with black
+        SCREEN.fill("black")
+        screen.blit(surface, (0, 0))
+        surface.fill(pygame.Color('black'))
+
+        # Create and update the BACK button
+        PLAY_BACK = Button(image=None, pos=(640, 650),
+                           text_input="BACK", font=get_font(50), base_color="White", hovering_color="Green")
+        PLAY_BACK.changeColor(PLAY_MOUSE_POS)
+        PLAY_BACK.update(SCREEN)
+
+        # Draw animated symbols
+        for symbols in symbol_chains:
+            for symbol in symbols:
+                symbol.draw('green')
+
+        # Update and draw rotating circle
+        rotating_circle.update()
+        rotating_circle.draw()
+
+        # Update and draw  rotating "BCT" text
+        rotating_text_BCT.update()
+        rotating_text_BCT.draw()
+
+        # Update  alpha value for fading effect
+        if not pygame.time.get_ticks() % 20 and alpha_value < 170:
+            alpha_value += 6
+            surface.set_alpha(alpha_value)
+
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
+                    main_menu()
+
+        # Update  display
+        pygame.display.update()
+######################################################################################
+# Function to handle the OPTIONS screen
+
+
+def PLAY2():
+    while True:
+        # Get mouse position
+        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
+
+        # Fill the screen with white
+        SCREEN.fill("white")
+
+        # Display the OPTIONS text
+        myApp = App()
+
+        # Update the display
+        pygame.display.update()
+
+######################################################################################
+# Function to handle the main menu
+
+
+def main_menu():
+    while True:
+        # Display background image
+        SCREEN.blit(BG, (0, 0))
+
+        # Get mouse position
+        MENU_MOUSE_POS = pygame.mouse.get_pos()
+
+        # Display menu
+        MENU_TEXT = get_font(100).render("Screen Saver", True, "#b68f40")
+        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        ABOUT_TEXT = get_font(50).render(
+            "BY Subham And Utsav", True, "#b68f40")
+        ABOUT_RECT = ABOUT_TEXT.get_rect(center=(640, 650))
+
+        # Create buttons
+        PLAY1_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 250),
+                              text_input="2D SS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        PLAY2_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(640, 400),
+                              text_input="3D SS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(640, 550),
+                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
+
+        # Display menu text and update button colors
+        SCREEN.blit(MENU_TEXT, MENU_RECT)
+        SCREEN.blit(ABOUT_TEXT, ABOUT_RECT)
+        for button in [PLAY1_BUTTON, PLAY2_BUTTON, QUIT_BUTTON]:
+            button.changeColor(MENU_MOUSE_POS)
+            button.update(SCREEN)
+
+        # Event handling
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if PLAY1_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    PLAY1()  # Call the PLAY1 function
+                if PLAY2_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    PLAY2()  # Call the PLAY2 function
+                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
+                    pygame.quit()
+                    sys.exit()
+
+        # Update the display
+        pygame.display.update()
+
+
+######################################################################################
+# Start the main menu loop
+main_menu()
